45548ff3f9f6158423ccd06e200307479687e80d
EndoNrak <endo.noriaki.64m@kyoto-u.jp>
2021-02-07 00:03:46 +0900

    feat: add menu descriptions and images

--- src/app/mock-menu-category.ts
+++ src/app/mock-menu-category.ts
@@ -4,5 +4,5 @@ export const MenuCategories: MenuCategory[] = [
   {id: 0, large_category: "whiskey", middle_categories: ["Japanese", "Irish", "Scotch", "Bourbon"], description: "ウイスキー初心者から上級者まであなたに合ったウイスキーをお届けします。"},
   {id: 1, large_category: "others", middle_categories: ["Beer", "Gin", "Rum"], description: "季節のカクテルなどもあります"},
   {id: 2, large_category: "non alcohol", middle_categories: [], description: "ノンアルコールカクテルもご用意しています。アルコールが苦手な方、お子様も安心！"},
-  {id: 3, large_category: "food", middle_categories: [], description: "ウイスキーと最高のマッチするナッツやチョコレートなどなど。たまにカレー"}
+  {id: 3, large_category: "food", middle_categories: ["snack", "desert", "curry"], description: "ウイスキーと最高のマッチするナッツやチョコレートなどなど。たまにカレー"}
 ]

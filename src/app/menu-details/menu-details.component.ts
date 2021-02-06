16a6474da82602ecfa38388c887a8e4b2d098983
EndoNrak <endo.noriaki.64m@kyoto-u.jp>
2021-02-06 23:20:05 +0900

    feat: add menu descriptions in menu details page

--- src/app/menu-details/menu-details.component.ts
+++ src/app/menu-details/menu-details.component.ts
@@ -11,6 +11,7 @@ export class MenuDetailsComponent implements OnInit {
   menu_details!: Menu[];
   @Input() active_large_category_id!: number;
   @Input() active_middle_category_id!: number;
+  public isOpenedMenuDescriptions!: boolean[];
 
   constructor(public menuService : MenuService) {
    }
@@ -20,9 +21,15 @@ export class MenuDetailsComponent implements OnInit {
 
   ngOnChanges(): void{
     this.getMenuDetails()
+    this.isOpenedMenuDescriptions = Array(this.menu_details.length).fill(false)
   }
 
   getMenuDetails(): void{
     this.menu_details = this.menuService.getMenuDetails(this.active_large_category_id, this.active_middle_category_id)
+  }
+
+  toggleMenuDescription(index: number): void{
+    this.isOpenedMenuDescriptions[index] = !this.isOpenedMenuDescriptions[index]
+    console.log(this.isOpenedMenuDescriptions)
   }
 }
